b4cebb7dd87b94238e2e39261d92ec75b2f0f18c
Justin Doak <justinhdoak@gmail.com>
2016-09-29 14:57:40 -0400

    Added footer with text and image.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,7 @@
 //= require jquery_ujs
 //= require_tree .
 //= require turbolinks
+//= require ./baconIpsum.js
 //= require ./bacon.js
 
 $(document).ready(function() {
--- app/assets/javascripts/bacon.js
+++ app/assets/javascripts/bacon.js
@@ -1,16 +1,17 @@
 $(document).ready(function() {
   // Use .ready to ensure turbolinks works properly
 
-  $.get('https://baconipsum.com/api/?type=all-meat&paras=1&start-with-lorem=1')
+  // Initializes bacon-ipsum box with AJAX call to Bacon Ipsum API
+  $.get('https://baconipsum.com/api/?type=meat-and-filler&paras=1&start-with-lorem=1')
     .done(function(data){
       $("#bacon-ipsum").html(data);
     });
 
+  // On button click: calls Bacon Ipsum API for new bacon-ipsum content
   $("#bacon-button").click(function() {
-    $.get('https://baconipsum.com/api/?type=all-meat&paras=1&start-with-lorem=1')
+    $.get('https://baconipsum.com/api/?type=meat-and-filler&paras=1&start-with-lorem=1')
       .done(function(data){
         $("#bacon-ipsum").html(data);
       });
     });
-
 })
