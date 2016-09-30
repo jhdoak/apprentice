4de47869268aa2cd60d97bc1f0308fc93c5fc27c
Justin Doak <justinhdoak@gmail.com>
2016-09-30 13:25:35 -0400

    Wrote for loop to set map markers on map.

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,15 +1,20 @@
 function initMap() {
 
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
+    center: {lat: 33.748995, lng: -84.387982},
     zoom: 8
   });
 
+  for (var i = 0; i < 10; i++) {
+  var latLng =
+    new google.maps.LatLng($('.locations').data('locations')[i].lat, $('.locations').data('locations')[i].lng);
+
+  var marker = new google.maps.Marker({
+    position: latLng,
+  });
+
+  marker.setMap(map);
 }
 
-$(document).ready(function() {
 
-})
-
-// $('.locations').data('locations')[0].lat
-// $('.locations').data('locations')[0].lng
+}
