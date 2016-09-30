a467949e7f57fe800270986fa8337ff64df5e464
Justin Doak <justinhdoak@gmail.com>
2016-09-30 10:38:40 -0400

    Added redirect to '/answers' from all unmatched paths.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
 
-  resources :faqs
+  # resources :faqs
 
   # Static Pages
   root to: 'static_pages#home'
   get 'home', to: 'static_pages#home', as: 'home'
   get 'answers', to: 'faqs#index', as: 'answers'
 
-  devise_for :users
+  # Send all unmatched paths to '/answers'
+  get '*path' => redirect('/answers')
+
+  # devise_for :users
 
 end
