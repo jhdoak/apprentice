cf4f4235bab385115b9128f0d57131a1026cc162
Justin Doak <justinhdoak@gmail.com>
2016-10-11 20:36:45 -0400

    Added /answers alias for all /faqs routes. Set 'edit answer' link to only show up for admin users.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
-  # resources :faqs
+  resources :faqs, path: 'answers'
 
   # Static Pages
   root to: 'static_pages#home'
   get 'map', to: 'static_pages#map', as: 'map'
-
-  get 'answers', to: 'faqs#index', as: 'answers'
-  get 'answers/edit', to: 'faqs#edit', as: 'answers/edit'
 
   devise_for :users
   get 'user_root', to: 'faqs#index', as: :user_root
